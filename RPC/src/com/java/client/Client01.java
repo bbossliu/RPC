fe2b7104fe37a6160fec7eb163985b959d3f0e7a
BBossLiu <1318071845@qq.com>
2019-06-24 12:07:40 +0800

    尝试解决冲突

--- RPC/src/com/java/client/Client01.java
+++ RPC/src/com/java/client/Client01.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-//helloClient
+//aaa
 public class Client01 {
 	
 	public static <T>T getREmoteProxyObj(Class serviceInterface,InetSocketAddress addressPort){
